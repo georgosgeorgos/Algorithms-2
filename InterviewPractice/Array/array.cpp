15cfaeb03ed7e1d0d2a72ebe128d294aeca6ca4c
Soon Chee Loong <scheeloong@gmail.com>
2015-10-01 00:58:46 -0700

    implemented finding pivot of a rotated sorted array with only distinct elements

--- InterviewPractice/Array/array.cpp
+++ InterviewPractice/Array/array.cpp
@@ -3,12 +3,37 @@
 /* //
 Table of Contents
 1. Maximum Subarray Problem using Kadane's Algorithm
+2. Sorted Shifted Array with only distinct elements, find the amount array is shifted by = find minimum of sorted shifted array 
+2. Sorted Shifted Array with only distinct elements, find the amount array is shifted by = find minimum of sorted shifted array, T(n) = O(logn) = S(n)
+TODO:
+// Sorted Shifted Array with duplicate elements, find the amount array is shifted by =
+// Sorted shifted array with only distinct elements, search for an element
+// Sorted shifted array with duplicate elements, search for an element
 
-// TODO: Sorted Shifted Array, find the amount array is shifted by
-// TODO:  Sorted shifted array, search for an element
-// TODO: Find the only singular integer in an array of pairs of integers
-// TODO: Array with n distinct elements and local minimums, find one local min in O(logN)
+// Buy and sell stocks as many times. But only 1 transaction at a time (must sell whatever bought before can buy again)
+   Max profit? buy and sell stock at most k times. Max profit? (k's base case is 1, k = 1)
+    - For many times, just do it recursively. Solve for k = 1, then divide left and right to 2 recursions 
+    - For k times, do similarly as many times, but use O(n) space by returning linked list of all the components of many times in sorted order
+    - Sorting takes O(n) since it's the merge step in merge sort at each recursion. 
+    - Then just pick the first k. 
+    (DONE IN LEETCODE)
+
+
+// CIRCULAR ARRAY: 
+    HouseRobberIIFromLeetCode: Find the maximum sum of non-adjacent elements in a circular array. 
+    e.g. [2,6,5,7,8] = 6+8 = 14 (can't pick both 2 and 8 in the sum as it is circular array and so 2 and 8 will be adjacent)
+    Time Complexity, T(n) = O(n) single pass, Space Complexity, S(n) = O(1)
+    Hint: Use dynamic programming
+    
+
+
+// Find the only singular integer in an array of pairs of integers (done in leetcode)
+// Array with n distinct elements and local minimums, find one local min in O(logN)
 e.g. 10, 6, 4, 3, 12, 19, 18, 20, 17 has 3 local mins at : 3, 18, 17
+// ADD Array questions implemented in RandomDecision for ECE345 assignments here, where you find intersection of 2 different arrays etc. 
+// Given circular array with reference indexing, return if it is a complete circular array.T(n) = O(n), S(n) = O(1) , (Google Practice Interview) note: Use a single pass only
+        // hint: Just check if nth element is the starting element. If it is not, then error, if you reach starting element before n, then error
+    
 Easy, just prove that if A[n-1] < A[n] , there must exist local min in A[1] -> A[n-1]
 Similarly for A[n-1] < A[n-2] => There must exist local min in A[n-1] -> A[n]
 note: Always check for A[n-1] itself being local min as base case cause
@@ -18,8 +43,26 @@ note: Always check for A[n-1] itself being local min as base case cause
 
 // TODO: 2. Merge two equally sized integer arrays, the first one   having the capacity to accommodate the result.
             Hint: Merge from end to first so won't overwrite non-merge values
-
 // TODO 3.   Implement a method for a Fibonacci sequence where instead of adding the last 2 elemenT's you add the last n elements. For n=3. 1 1 1 3 5 9...
+//----------------------------------------------------------------------------------------
+// Common Hints
+//-------------------------
+- Have 2 pointers, one at each end and move towards the middle 
+- Sort and use binary search 
+- Counting sort and Radix sort 
+- Solve for 1, then recursively solve the remainders  
+- Have 2 pointers, one at each end, have third pointer moving from one end to another and swapping elements in and out from both end pointers. 
+- Partition values to left and right of middle element (quicksort) 
+//-------------------------
+// Common Questions
+//-------------------------
+- (-) index? 
+- resizable or fixed size? 
+- the type the array is holding? 
+- circular or straight array? 
+- Traverse from left to right, then traverse from right to left. 
+    (e.g. Product of entire array except itself without using division(/) operator) 
+    (e.g. Sum of entire array except itself without using deduct (-) operator) 
 // */
 //----------------------------------------------------------------------------------------
 /* //
@@ -147,5 +190,87 @@ int maxSubArrayDynamic(int* a, int N)
     else return maxSum; 
 }
 // */
+//----------------------------------------------------------------------------------------
+// 2 Sorted Shifted Array with only distinct elements, find the amount array is shifted by = find minimum of sorted shifted array 
+// Time Complexity, T(n) = O(logn), S(n) = O(logn)
+//------------------------
+/*
+Questions: 
+Can duplicates exist?  NO
+Can the values be (-)? can the integer be any values? 
+what is maximum length of array ?
+Does [-1] indexing work on this rotated array? 
+What if array is empty?  return -1 
+Do i output the minimum value or its index? 
+
+Function Prototype 
+
+int getPivot(vector<int>& nums); 
+
+TEST_CASE: 
 
 
+	4567012 => return 4 (indexof 0) 
+	1 => return 0 
+	1,2=> return 0
+	2,1 => return 1
+	2,1,3 
+	3,3,1,3 => returns 2 (CODE DOESNT WORK FOR THIS CASE!) 
+
+ALGORITHM: 
+	Binary search. 
+	// base case
+	If nums[mid] < nums[mid-1] return mid  
+	if (nums[mid] > nums[end])
+		begin = mid+1
+		end = end 
+		Search 
+	else 
+		begin = begin
+		end = mid-1
+		search 
+
+Implementation: 
+TEST!
+*/
+//------------------------
+/* //
+#include <vector> 
+#include <iostream> 
+using namespace std; 
+
+int binarySearch(vector<int>& nums, int begin, int end)
+{
+	int mid = ((end + begin)/2); 
+	// Base cases
+	if((mid > begin && nums[mid] < nums[mid-1]) || (mid == begin && nums[mid] <= nums[end]))     
+		return mid; 
+    // General case 
+    if (nums[mid] > nums[end]) 
+    {
+        begin = mid+1; 
+        return binarySearch(nums, begin, end); 
+    }
+    else
+    {
+        end = mid -1; 
+        return binarySearch(nums, begin, end); 
+    }	
+} 
+
+int getPivot(vector<int>& nums)
+{
+	if( nums.size() == 0) return -1; 
+	int result = binarySearch(nums, 0, nums.size()-1); 
+	return result; 
+}
+
+int main(void)
+{
+	vector<int> nums = {4,5,6,7,0,1,2}; // C++ 11 compiler required 
+	int result = getPivot(nums); 
+	cout << result << endl; 
+	cout << nums[result] << endl; 
+}
+// */
+//----------------------------------------------------------------------------------------
