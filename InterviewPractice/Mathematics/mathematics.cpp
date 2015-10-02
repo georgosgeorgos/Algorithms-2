41486364deef17f26b470c1516f204c4426b063d
Soon Chee Loong <scheeloong@gmail.com>
2015-10-02 07:47:26 -0700

    Implemented Euclid's Algorithm for gcd

--- InterviewPractice/Mathematics/mathematics.cpp
+++ InterviewPractice/Mathematics/mathematics.cpp
@@ -5,6 +5,8 @@ Table of Contents
 
 1. Calculate pow(double, int n), where n can be (-), 0 , (+)  T(n) = O(logN), S(n) = O(logN)
 
+2. Euclid's Algorithm: Calculate GCD, T(n) = O(n), S(n) = O(1)
+
 //----------------------------------------------------------------------------------------
 TODO:
 0. Calculate pow(double, int n), where n can be (-), 0 , (+)  T(n) = O(logN), S(n) = O(1) Hint: Dynamic Programming
@@ -14,6 +16,23 @@ TODO:
 
 3. Generate prime numbers up to N efficiently: Sieve of Eratosthenes
 4. Look up sorting integers in O(logN) time on some Youtube MIT video on advanced data structures
+5. Given numbers 0,1,2,...,N inserted into an array. Now exactly 1 number is removed from this array. Find that one number. 
+6. Given numbers 0,1,2,...,N inserted into an array. Now exactly 2 numbers are removed from this array. Find these two number. 
+    Hint: Linear equation and quadratic eqn. 
+7. Given numbers 0,1,2,...,N inserted into an array. Now exactly k numbers are removed from this array. Find these k numbers, k <= n 
+8. Given a value N, find the minimum number perfect squares sums to reach n. e.g. f(13) = 2 (2*2 + 3*3 = 4 + 9)
+hint: Dynamic Programming/BFS/Math theorem can be used to solve
+
+9. Reverse Integer 
+    -321 => -123
+    T(n) = O(n) S(n) = O(1)
+
+10. Determine whether an integer is a paindrome 
+    12321 => True
+    -12321 => False
+    T(n) = O(n) S(n) = O(1)
+
+11. 
 
 
 // */
@@ -72,3 +91,72 @@ double power(double a, int n)
 }
 // */
 //----------------------------------------------------------------------------------------
+// 2 Euclid's Algorithm: Calculate GCD  
+//----------------------------------------------------------------------------------------
+// Time Complexity, T(n) = O(n)
+// Space Complexity, S(n) = O(1)
+#include <iostream>
+using namespace std;
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return;
+}
+// Based on Euclid's algorithm
+int gcd(int a, int b)
+{
+    int temp;
+    if (a > b)
+    {
+        swap(&a, &b);
+    }
+    // assume a <= b always
+    while (a != 0)
+    {
+        temp = a; // save the current lowest
+        a = b%a;  // get the new lowest
+        b = temp; // restore the previous lowest
+    }
+    return b; // return the lowest that resulted in the mod to be 0
+}
+
+int main(void)
+{
+    int T, N;
+    cin >> T;
+    int i = 0;
+    for(i = 0; i < T; i++)
+    {
+        cin >> N;
+        int values[N];
+        int j = 0 ;
+        cin >> values[0];
+        int gcdValue = values[0];
+        for (j = 1; j < N; j++)
+        {
+            cin >> values[j];
+            gcdValue = gcd(values[j],gcdValue);
+            if (gcdValue == 1)
+            {
+                cout << "YES" << endl;
+                // flush everything on current line
+                // Note: Need to add j++ at beginning of for loop to iterate to next iteration.
+                for (j++ ; j < N; j++)
+                {
+                    cin >> values[j];
+                    // will break here
+                }
+            }
+        }
+        if (gcdValue != 1)
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
+// */
+//----------------------------------------------------------------------
