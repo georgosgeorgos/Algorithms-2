741a3ee8396f54176f2e22dc4f4694998afdaa72
Soon Chee Loong <scheeloong@gmail.com>
2015-10-11 20:50:08 -0700

    Implemented Rod Cutting and removed old long implementation of rod cutting

--- InterviewPractice/DynamicProgramming/dynamicprogramming.cpp
+++ InterviewPractice/DynamicProgramming/dynamicprogramming.cpp
@@ -2,8 +2,8 @@
 /*
 Table of Contents
 1. Fibonacci Sequence (Bottom Up), T(n) = O(n), S(n) = O(n)
-2. Rod Cutting Problem (Top Down with Memoization),T(n) = O(n^2), S(n) = O(n)
-3. Rod Cutting Problem (Bottom Up), T(n) =  O(n^2) ,S(n) =  O(n)
+2. Rod Cutting Problem: Given a rod of length n, and a price for each length of rod, find ways to cut the rod to have maximum value, T(n) = O(n^2), S(n) = O(n)
+// TODO: 3
 4. Longest Common Subsequence Between Two Strings, T(n,m) = O(nm), S(n,m) = O(nm)
 5. Longest Increasing Subsequence, T(n) = O(n^2), S(n) = O(n)
 6. Edit Distance, given 2 strings that are different, find min. number of changes to convert 1 to another, T(n,m) = O(nm), S(n,m) = O(nm)
@@ -194,185 +194,50 @@ int fibonacci(int n, int* p)
     return p[n] ;
 }
 // */
-
 //----------------------------------------------------------------------------------------------------
-// 2 Rod Cutting Problem ( Dynamic Programming)
-// Top-Down with memoization
-// Given a rod of length n, and a price for rod of length 1, 2, ... n.
-// Determine how to cut the rod to maximize total price.
-// Note: There are 2^n - 1 combinations of possible answers since
-//       For every i > 1, you can decide if you wanna cut, or not cut.
-//  Which means trying everything out will be an exponential time
-// Method 1: Top-Down with memoization
-// Time Complexity, T(n) = O(n^2) , using dynamic programming
+// 2 Rod Cutting Problem: Given a rod of length n, and a price for each length of rod, find ways to cut the rod to have maximum value
+// Time Complexity, T(n) = O(n^2)
 // Space Complexity, S(n) = O(n)
-// Note: Use dynamic programming and print the final solution, not just the optimal value
-// TODO: Have not printed the final solution
+//-------------------------------------
+/*
+Questions
+    1. 
+*/
 //-------------------------------------
 /* //
-#include <iostream>
-#include <stdlib.h>
-#include <limits.h>
+#include <vector> 
+#include <iostream> 
+using namespace std; 
 
-using namespace std;
-int cut_rod(int *p , int n, int *s, int *r);
-int main(void)
+int RodCutting(vector<int> rodPrices)
 {
-    int *p; // prices
-    int *s; // list of cuts for printing
-    int *r; // current list from solution
-    int n = 10;
-    p = (int *) malloc(sizeof(int) * (n+1));
-    s = (int *) malloc(sizeof(int) * (n+1));
-    r = (int *) malloc(sizeof(int) * (n+1));
-    p[0] = 0;
-    p[1] = 1;
-    p[2] = 5;
-    p[3] = 8;
-    p[4] = 9;
-    p[5] = 10;
-    p[6] = 17;
-    p[7] = 17;
-    p[8] = 20;
-    p[9] = 24;
-    p[10]= 30;
-    int  i = 0;
-    for ( i = 0; i <= n; i++)
+    int n = rodPrices.size();
+    if (n <= 0) return 0;
+    vector<int> L (n, 0);
+    // Initialize base case for all rod of length 1
+    for(int i = 0; i < n; i++)
     {
-        r[i] = -1;
-    }
-    for (i =0 ; i <= n; i++)
-    {
-        int k = cut_rod(p, i, s, r);
-        cout << "Optimal value for " << i << " is " << k << endl;
+        L[i] = rodPrices[i];
     }
 
-    return 0;
-}
-
-int cut_rod(int *p , int n, int *s, int *r)
-{
-    if (n == 0)
+    for(int i = 1; i < n; i++)
     {
-        r[0] = 0;// COMMENT THIS OUT FOR PURELY RECURSIVE WITHOUT MEMOIZATION
-        p[0] = 0;
-        return 0;
-    }
-    if (r[n] >= 0) return r[n];// COMMENT THIS OUT FOR PURELY RECURSIVE WITHOUT MEMOIZATION
-    int i = 0;
-    int q = INT_MIN;
-    for (i = 1; i <= n; i++)
-    {
-                // max(whatever stored before, p[i] + r[n-i]
-                // You can use p[i] cause you know that you are accounting for all cases of
-                // the piece of each length from 1 all the way up to n.
-                // this means you would have accounted for all cases.
-        // Refer to pg 362/1292 of actual textbook from CLRS 3rd ed, it doesn't provide the proof
-        // but it says you can prove it.
-        q = max(q, p[i] + cut_rod(p, n-i, s, r));
-    //    q = max(q, cut_rod(p, i, s, r) + cut_rod(p, n-i, s, r)); // NOTE: This is wrong and causes segfault.
-    //    cout << "q is " << q << endl;
-    }
-    r[n] = q;  // COMMENT THIS OUT FOR PURELY RECURSIVE WITHOUT MEMOIZATION
-    return q;
-}
-// */
-//----------------------------------------------------------------------------------------------------
-// 3 Rod Cutting Problem ( Dynamic Programming)
-// Bottom Up Approach
-// Given a rod of length n, and a price for rod of length 1, 2, ... n.
-// Determine how to cut the rod to maximize total price.
-// Note: There are 2^n - 1 combinations of possible answers since
-//       For every i > 1, you can decide if you wanna cut, or not cut.
-//  Which means trying everything out will be an exponential time
-// Method 2: Bottom Up Approach
-// Time Complexity, T(n) =  O(n^2) , using dynamic programming
-// Space Complexity, S(n) =  O(n)
-// Note: Use dynamic programming and print the final solution, not just the optimal value
-//-------------------------------------
-/* //
-#include <iostream>
-#include <stdlib.h>
-#include <limits.h>
-
-using namespace std;
-int cut_rod(int *p , int n, int *s, int *r);
-int main(void)
-{
-    int *p; // prices
-    int *s; // list of cuts for printing
-    int *r; // current list from solution
-    int n = 10;
-    p = (int *) malloc(sizeof(int) * (n+1));
-    s = (int *) malloc(sizeof(int) * (n+1));
-    r = (int *) malloc(sizeof(int) * (n+1));
-    p[0] = 0;
-    p[1] = 1;
-    p[2] = 5;
-    p[3] = 8;
-    p[4] = 9;
-    p[5] = 10;
-    p[6] = 17;
-    p[7] = 17;
-    p[8] = 20;
-    p[9] = 24;
-    p[10]= 30;
-    int  i = 0;
-    for ( i = 0; i <= n; i++)
-    {
-        r[i] = -1;
-    }
-    for (i =0 ; i <= n; i++)
-    {
-        int k = cut_rod(p, i, s, r);
-        cout << "Optimal value for " << i << " is " << k << endl;
-    }
-
-    return 0;
-}
-
-int cut_rod(int *p , int n, int *s, int *r)
-{
-    r[0] = 0;
-    p[0] = 0;
-    if (n == 0)
-    {
-        return 0;
-    }
-    int i = 0;
-    int j = 0;
-    for (i = 1; i <= n; i++)
-    {
-        int q = INT_MIN;
-        for (j = 1; j <= i; j++)
+        for(int j = 0; j <= i/2; j++)
         {
-            // max(whatever stored before, p[i] + r[n-i]
-            // You can use p[i] cause you know that you are accounting for all cases of
-            // the piece of each length from 1 all the way up to n.
-            // this means you would have accounted for all cases.
-            // Refer to pg 362/1292 of actual textbook from CLRS 3rd ed, it doesn't provide the proof
-            // but it says you can prove it.
-            int oldq = q;
-            q = max(q, p[j] + r[i-j]); // note: NO recursion
-            if (oldq != q)
-            {
-                s[i] = j;
-            }
+            L[i] = max(L[i], L[j] + L[i-j-1]);
         }
-        r[i] = q;
-
-    //    q = max(q, cut_rod(p, i, s, r) + cut_rod(p, n-i, s, r)); // NOTE: This is wrong and causes segfault.
-    //    cout << "q is " << q << endl;
     }
-    int l = n;
-    cout <<"Arrangement for " << n << " is: " <<endl;
-    while ( l > 0)
-    {
-        cout << "r[" << s[l] << "] ";
-        l = l - s[l];
-    }
-    cout << endl;
-    return r[n];
+    return L[n-1];
+}
+int main(void)
+{
+    vector<int> rodPrices = {0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 23}; // C++11 feature
+    int maxValue = RodCutting(rodPrices); 
+    cout << maxValue << endl; // 25
+    vector<int> rodPrices2 = {5};
+    maxValue = RodCutting(rodPrices2); 
+    cout << maxValue << endl; // 5 
+    return 0;
 }
 // */
 //----------------------------------------------------------------------------------------------------
