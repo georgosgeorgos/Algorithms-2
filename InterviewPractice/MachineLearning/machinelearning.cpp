4c56e3a3de67272a1657926a9b4d54eac278709e
scheeloong <scheeloong@gmail.com>
2016-02-28 20:39:25 -0800

    Added a huge list of ML algorithms

--- InterviewPractice/MachineLearning/machinelearning.cpp
+++ InterviewPractice/MachineLearning/machinelearning.cpp
@@ -1,4 +1,76 @@
 /*
+
+Algorithms:
+    Regression = measure of error in prediction made by model
+        Ordinary Least Square Regression (OLSR)
+        Linear Regression
+        Logistic Regression
+        Stepwise Regression
+        Multivariate Adaptive Regression Splines (MARS) 
+        Locally Estimated Scatterplot Smoothing
+    Instance-based = Similarities between new data and model database
+        k-Nearest Neighbour (kNN)
+        Learning Vector Quantization
+        Self-Organizing Map
+        Locally Weighted Learning
+    Regularization 
+        Ridge Regression
+        Least Absolute Shrinkage and Selection Operator (LASSO)
+        Elastic Net
+        Least-Angle Regression
+    Decision Tree
+        Classification and Regression Tree (CART) 
+        Iterative Dichotomiser 3 (ID3)
+        Chi-squared Automatic Interaction Detection (CHAID)
+        Decision Stump
+        M5
+        Conditional Decision Trees
+    Bayesian 
+        Naive Bayes
+        Gaussian Naive Bayes
+        Multinomial Naive Bayes
+        Averaged One-Dependence Estimators (AODE)
+        Bayesian Belief Network (BBN)
+        Bayesian Network (BN)
+    Clustering
+        k-Means
+        k-Medians
+        Expectation Maximisation (EM) 
+        Hierarchical Clustering
+    Association Rule Learning
+        Apriori Algorithm
+        Eclat Algorithm
+    Artificial Neural Network
+        Perceptron
+        Back-Propagation
+        Hopfield Network
+        Radial Basis Function Network
+    Deep Learning 
+        Deep Boltzmann Machine (DBM)
+        Deep Belief Networks (DBN)
+        Convolutional Neural Network (CNN)
+        Stacked Auto-Encoders
+    Dimensionality Reduction
+        Principal Component Analysis
+        Principal Component Regression
+        Partial Least Square Regression (PLSR)
+        Sammon Mapping
+        Multidimensional Scaling (MDS) 
+        Projection Pursuit
+        Linear Discriminant Analysis (LDA)
+        Mixture Discriminant Analysis (MDA) 
+        Quadratic Discriminant Analysis (QDA)
+        Flexible Discriminant Analysis (FDA)
+    Ensemble
+        Boosting
+        Bootstrapped Aggregation (Bagging)
+        AdaBoost
+        Stacked Generalization (blending)
+        Gradient Boosting Machines (GBM)
+        Gradient Boosted Regression Trees (GBRT)
+        Random Forest
+    Support Vector Machines
+    Probabilistic Graphical Models
 TODO:
 Support Vector Machines
 Convolutional Neural Networks 
