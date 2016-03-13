38cb50515800debbe5dec78573f087249ac505d5
scheeloong <scheeloong@gmail.com>
2016-03-13 06:14:14 -0400

    Added programming paradigm

---
+++ InterviewPractice/ProgrammingParadigm/programmingparadigm.cpp
@@ -0,0 +1,50 @@
+Programming Paradigm
+//-----------------------------------------------------------------------------------------------------------
+Table Of Contents
+//-----------------------------------------------------------------------------------------------------------
+    Imperative Programming = Every command shows how programming takes place, step by step
+    Structured = No goto statements, replaced with functions instead
+    Procedural = Sequence of instructions
+    Object-Oriented = Everything is an object
+    Declarative = Program state the result you want, not how to get it.
+    Procedural
+    Object-Oriented
+    Asynchronous
+    Functional
+    Logic = Uses or and and
+//-----------------------------------------------------------------------------------------------------------
+Imperative Programming = Every command shows how programming takes place, step by step
+    - Uses lots of goto (to move to a subroutine) 
+    - Languages: Assembly
+//-----------------------------------------------------------------------------------------------------------
+Structured = No goto statements, replaced with functions instead
+    - Languages: C
+//-----------------------------------------------------------------------------------------------------------
+Procedural = Sequence of instructions
+    - Languages: C
+//-----------------------------------------------------------------------------------------------------------
+Object-Oriented = Everything is an object
+    - Languages: Java, C++
+//-----------------------------------------------------------------------------------------------------------
+Declarative = Program state the result you want, not how to get it.
+    - Languages: SQL
+//-----------------------------------------------------------------------------------------------------------
+Logic =  Uses or and and
+    - Languages: Prolog
+//-----------------------------------------------------------------------------------------------------------
+
+Functional (Applicative) — Computation proceeds by (nested) function calls that avoid any global state.
+Function-Level (Combinator) — Programs have no variables. No kidding.
+Object-Oriented — Computation is effected by sending messages to objects; objects have state and behavior.
+Class-based — Objects get their state and behavior based on membership in a class.
+Prototype-based — Objects get their behavior from a prototype object.
+Event-Driven — Control flow is determined by asynchronous actions (from humans or sensors).
+Flow-Driven — Computation is specified by multiple processes communicating over predefined channels.
+Logic (Rule-based) — Programmer specifies a set of facts and rules, and an engine infers the answers to questions.
+Constraint — Programmer specifies a set of constraints, and an engine infers the answers to questions.
+Aspect-Oriented — Programs have cross-cutting concerns applied transparently.
+Reflective — Programs manipulate their own structures.
+Array — Operators are extended to arrays, so loops are normally unnecessary.
+
+
+    
