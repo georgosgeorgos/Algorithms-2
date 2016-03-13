12f3475d495978ed61171e7992e5f59fe8cd80a4
scheeloong <scheeloong@gmail.com>
2016-03-13 05:11:39 -0400

    Added functional programming

---
+++ InterviewPractice/CleanCode/functionalprogramming.cpp
@@ -0,0 +1,26 @@
+Functional Programming
+    - Imposes restrictions on how to write program, while maintaining what programs we can write
+
+Pure Functions
+    - no hidden inputs and hidden outputs
+Writing code that doesn't change state
+    - Can call same function multiple times
+        - Easier to parallelize
+    - Same input => Same output 
+    - Easier to test
+    - Less prone to bugs
+    - Easier to prove for correctness
+Pass function as parameters to other functions
+
+- Reduce hidden inputs and hidden outputs to conform with some of functional programming's specialties
+    Hidden inputs => Inputs that function depends on, but isn't pass in as parameter
+        - state of global/static variable
+        - Current time
+        - Reading user input
+        - Reading from file
+    Hidden outputs => Outputs that function changes but isn't in the return by reference/value.
+        - changing state of global/static variable
+        - changing state of input variable in-place
+        - sending output to ethernet, audio, file, screen
+        - Throwing exception
+        - Halting with errors
