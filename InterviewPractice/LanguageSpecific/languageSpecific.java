7465e5849d4c365a23da05a555ea1bb01119c2e0
scheeloong <scheeloong@gmail.com>
2016-06-11 21:49:13 -0400

    Added using .equals operator with strings

--- InterviewPractice/LanguageSpecific/languageSpecific.java
+++ InterviewPractice/LanguageSpecific/languageSpecific.java
@@ -3,6 +3,8 @@
 // Strings
 //-------------------------
 1. Never concatenate strings in a Loop
+2. Compare string using .equals NOT ==
+3. Compare string from constant to variable
 
 */
 //----------------------------------------------------------------------------------------------------------------------------------
@@ -25,4 +27,18 @@ String s = builder.toString();
 
 However, it is fine if you use it in a line as it is turned into StringBuilder by compiler automatically 
 String s = "1, " + "2, " + "3, " + "4";
+//----------------------------------------------------------------------------------------------------------------------------------
+// 2 Compare string using .equals NOT ==
+//---------------------------------
+String a = "hello";
+String b = "hello";
+assertFalse(a == b); // not same object
+assertTrue(a.equals(b));
+//----------------------------------------------------------------------------------------------------------------------------------
+// 3 Compare string from constant to variable
+//---------------------------------
+from:
+    if(b.equals("hello")) // a runtime exception if b == NULL
+to:
+    if("hello".equals(b)) // handles if b == NULL case
 //----------------------------------------------------------------------------------------------------------------------------------
