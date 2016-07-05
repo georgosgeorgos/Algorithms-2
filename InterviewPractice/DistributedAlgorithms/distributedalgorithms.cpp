3762a21b9f0f5b24a595c02238c6b242cbb1275c
scheeloong <scheeloong@gmail.com>
2016-07-05 17:59:39 -0400

    Added TODO

--- InterviewPractice/DistributedAlgorithms/distributedalgorithms.cpp
+++ InterviewPractice/DistributedAlgorithms/distributedalgorithms.cpp
@@ -1,8 +1,43 @@
 //----------------------------------------------------------------------------------------------------------------------------------
 /* //
 Table of Contents
+Message Complexity, M(n) = Total number of messages exchanged
+Bit Complexity, B(n) = Total number of bits flipped (useful if messages are long)
 //-------------------------
 TODO: 
-1. Paxos Algorithm
+101. Paxos Algorithm
+Snapshot = Get current configuration
+102. Chandy-Lamport Snapshot Algorithm
+103. Lai-Yang Snapshot Algorithm 
+Wave = Broadcast message and get a response from all members
+104. Tarry's Wave Algorithm
+105. Distributed Depth First Search
+106. Awerbuch's Wave Algorithm
+107. Cidon's Wave Algorithm
+108. Echo Algorithm
+Deadlock Detection
+109. Wait-for graphs
+110. Bracha-Toueg 
+Termination Detection
+111. Dijikstra-Scholten Algorithm 
+112. Weight Throwing Algorithm
+113. Rana's Algorithm
+114. Safra's Algorithm
+Routing - Sending a message to another node that is not a direct neighbour
+115. Chandy-Misra Algorithm
+116. Merlin-Segall Algorithm 
+117. Toueg Algorithm
+118. Frederickson Algorithm
+119. Packet Switching
+Election 
+120. Chang-Roberts Algorithm
+121. Franklin's Algorithm
+122. Dolev-Klawe-Rodeh Algorithm
+123. Gallager-Humblet-Spira Algorihtm
+124. Tree Election Algorithm
+125. Echo Algorithm with Extinction
+Anonymous Networks = Processes that do not know other processes' ID
+126. Itai-Rodeh Ring Size Algorithm
+Refer to book by Wan Fokkik and Nancy Lynch for more details
 */
 //----------------------------------------------------------------------------------------------------------------------------------
