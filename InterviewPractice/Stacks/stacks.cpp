00b6efd1088844a75ede2a423864db291e98030d
scheeloong <scheeloong@gmail.com>
2016-07-11 18:20:27 -0400

    Reformatted

--- InterviewPractice/Stacks/stacks.cpp
+++ InterviewPractice/Stacks/stacks.cpp
@@ -1,10 +1,9 @@
 //
-//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------
 /* //
 Table of Contents
 0. Implement a class for Stacks using (Array, LinkedList) and comparisons, T(n) = O(1), S(n) = O(n)
-1. Introduction to Stacks with C++ Standard Template Library, T(n) = O(1), S(n) = O(1)
-2. Stack that can return mininum value element efficiently, T(n) = O(1), S(n) = O(n). (Bloomberg 2nd round)
+1. Stack that can return mininum value element efficiently, T(n) = O(1), S(n) = O(n). (Bloomberg 2nd round)
 
 TODO:
 3. Find out if the braces, brackets open and close properly (Cast Software Interview)
@@ -18,9 +17,9 @@ or when you add a value lower than current min, you push to the 2nd array's min.
 7. Implement 3 stacks in one array, T(n) = O(...), S(n) = O(n)
     // Note: basically every stack function has an additional stackNumber parameter. 
 // */
-//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------
+// Introduction to Stacks with C++ Standard Template Library, T(n) = O(1), S(n) = O(1)
 /* //
-// 1 Introduction to Stacks with C++ Standard Template Library
 #include <iostream>
 #include <stack>
     // s.push(value)
@@ -30,9 +29,7 @@ or when you add a value lower than current min, you push to the 2nd array's min.
     // s.empty()
     // s1.swap(s2)
         // swap 2 stacks with each other
-
 using namespace std;
-
 int main(void)
 {
     // values of the stack
@@ -53,12 +50,104 @@ int main(void)
     cout << "Number of elements in stack is " << s.size() << endl;
     return 0;
 }
+
 // */
-//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------
+// 0 Implement a class for Stacks (Array, LinkedList) and comparisons
+//-------------------------
+/*
+// Approach 1: Array
+// Approach 2: Linked List
+    // Since a Stack essentially only lets you access top element and push and pop from the top only,
+    // you do not need the indexing capabilities of an array. Therefore, a Linked List is a definite better choice of implementation. 
+    // Linked List also reduces time complexity of growing and shrinking the stack.
+        // Time Complexity, T(n) = O(1)
+        // Space Complexity, S(n) = O(n)
+    // Note: Approach 2 has same complexity for all methods below:
+        // push()
+        // pop()
+        // top()
+        // empty()
+        // size()
+    // Approach 1's push may require growing stack and pop may require shrinking stack which takes more than constant time and more memory than needed. 
+*/
+//-------------------------
 /* //
-// 2  Stack that can return the minimum value element efficiently.  
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+struct node {
+    int value;
+    struct node* next;
+};
+
+class ownStack {
+private:
+    struct node* top;
+    int size;
+public:
+    ownStack();
+    void push(int value);
+    void pop();
+    // Mistake: cannot name a function the same name as one of its variables
+    int getTop();
+    bool empty();
+};
+ownStack::ownStack() 
+{
+    this->size = 0;
+    this->top = NULL;
+}
+
+void ownStack::push(int value)
+{
+    struct node* newNode = (struct node *) malloc(sizeof(struct node));
+    newNode->value = value;
+    newNode->next = this->top;
+    this->top = newNode;
+    this->size++;
+}
+void ownStack::pop()
+{
+    if(this->size == 0) return;
+    this->size--;
+    struct node* delNode = this->top;
+    this->top = this->top->next;
+    free(delNode);
+}
+int ownStack::getTop()
+{
+    if(this->size == 0) return -1;
+    return top->value;
+}
+bool ownStack::empty()
+{
+    if(this->size == 0) return true;
+    return false;
+}
+
+int main(void)
+{
+    ownStack s; 
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    while(!s.empty())
+    {
+        cout << s.getTop() << " " << endl;
+        s.pop();
+    }
+    return 0;
+}
+// */
+//----------------------------------------------------------------------------------------------------------------------------------
+// 1 Stack that can return the minimum value element efficiently.  
 // Time Complexity, T(n) = O(1)
 // Space Complexity, S(n) = O(n)
+//-------------------------
+/* //
 #include <cstdio> 
 #include <cstdlib> 
 #include <iostream> 
@@ -141,91 +230,4 @@ int main(void)
     return 0;
 }
 // */
-//----------------------------------------------------------------------------------------
-/* //
-// 0 Implement a class for Stack 
-// Approach 1: Array
-// Approach 2: Linked List
-    // Since a Stack essentially only lets you access top element and push and pop from the top only,
-    // you do not need the indexing capabilities of an array. Therefore, a Linked List is a definite better choice of implementation. 
-    // Linked List also reduces time complexity of growing and shrinking the stack.
-        // Time Complexity, T(n) = O(1)
-        // Space Complexity, S(n) = O(n)
-    // Note: Approach 2 has same complexity for all methods below:
-        // push()
-        // pop()
-        // top()
-        // empty()
-        // size()
-    // Approach 1's push may require growing stack and pop may require shrinking stack which takes more than constant time and more memory than needed. 
-    
-#include <cstdio>
-#include <cstdlib>
-#include <iostream>
-using namespace std;
-
-struct node {
-    int value;
-    struct node* next;
-};
-
-class ownStack {
-private:
-    struct node* top;
-    int size;
-public:
-    ownStack();
-    void push(int value);
-    void pop();
-    // Mistake: cannot name a function the same name as one of its variables
-    int getTop();
-    bool empty();
-};
-ownStack::ownStack() 
-{
-    this->size = 0;
-    this->top = NULL;
-}
-
-void ownStack::push(int value)
-{
-    struct node* newNode = (struct node *) malloc(sizeof(struct node));
-    newNode->value = value;
-    newNode->next = this->top;
-    this->top = newNode;
-    this->size++;
-}
-void ownStack::pop()
-{
-    if(this->size == 0) return;
-    this->size--;
-    struct node* delNode = this->top;
-    this->top = this->top->next;
-    free(delNode);
-}
-int ownStack::getTop()
-{
-    if(this->size == 0) return -1;
-    return top->value;
-}
-bool ownStack::empty()
-{
-    if(this->size == 0) return true;
-    return false;
-}
-
-int main(void)
-{
-    ownStack s; 
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    while(!s.empty())
-    {
-        cout << s.getTop() << " " << endl;
-        s.pop();
-    }
-    return 0;
-}
-// */
-//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------------------
