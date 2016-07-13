194315e4451d2cf0f4aed4f54e3bde4f9902dddf
scheeloong <scheeloong@gmail.com>
2016-07-13 17:49:30 -0400

    Added notes for style

--- InterviewPractice/CleanCode/styleguide.cpp
+++ InterviewPractice/CleanCode/styleguide.cpp
@@ -1,5 +1,7 @@
 //----------------------------
 // Soon Chee Loong's personal style guide for C++
+// Save header files with .h extension
+// Save C++ files with .cpp extension
 //----------------------------
 from:
     // Saves more lines, Java style
@@ -34,12 +36,26 @@ to:
     }
 //----------------------------
 from:
-    if(expression)
-    while(expression)
-    for(expression)
-    else if(expression)
+    // Able to look the same across different computers as long as same size monitors
+    indentUsingSpaces;
+to:
+    // Less to type since it's one keystroke versus many space strokes.
+    // Similarly, less keystrokes when hitting backspace
+    indentUsingTabs
+//----------------------------
+from:
+    if( expression )
+    while( expression )
+    for( expression )
+    else if( expression )
 to:
     // Clearer with just an extra space
+    if ( expression )
+    while ( expression )
+    for ( expression )
+    else if ( expression )
+to:
+    // Don't need extra space within '()' as it is clearer without
     if (expression)
     while (expression)
     for (expression)
@@ -48,35 +64,35 @@ to:
 from:
     // As maintainer, easier to see what is used by public before looking at public
     class A {
-        private:
-        public:
+     private:
+     public:
     };
 to:
     // As user, easier to just look at API provided from public and ignore details from private.
     class A {
-        public:
-        private:
+     public:
+     private:
     };
 //----------------------------
 from:
     // Variables before methods.
     class A {
-        public:
-            int memberVariableA;
-            void funcA();
-        private:
-            int memberVariableB;
-            void funcB();
+     public:
+        int memberVariableA;
+        void funcA();
+     private:
+        int memberVariableB;
+        void funcB();
     };
 to: 
     // Will always prefer re-using existing methods than accessing variables directly, so list method definitions first
     class A {
-        public:
-            void funcA();
-            int memberVariableA;
-        private:
-            void funcB();
-            int memberVariableB;
+     public:
+        void funcA();
+        int memberVariableA;
+     private:
+        void funcB();
+        int memberVariableB;
     };
 //----------------------------
 from:
@@ -135,6 +151,88 @@ to:
         functionB1();
     }
 //----------------------------
-Save header files with .h extension
-Save C++ files with .cpp extension
+from:
+    int var_name;
+    using type_name = int;
+    int member_variables_;
+    void FunctionNames();
+    namespace parent_a
+    {
+        namespace child_a
+        {
+            codeIndentedFromNamespace;
+        }
+    }
+to:
+    // Shorter to type
+    int varName;
+    using typeName = int;
+    int memberVariables; // access using this->memberVariables
+    void functionNames();
+    namespace parentA
+    {
+    namespace childA // access using parentA::childA::whateverItHas;
+    {
+    codeSameLineAsNamespace; // may have a lot of nested namespaces, no point indenting code from namespace
+    // Always have the closing comment for namespace so it's clear as may have lots of nested namespaces
+    } // namespace childA
+    } // namespace parentA
+//----------------------------
+from:
+    // Problem: using 'k' is a Google Style Guide convention, people outside Google wouldn't know.
+    static const int kConstantNames;
+to:
+    // easy to know it's a constant and doesn't change since it's all capitals
+    // Also, need to use '_' to read since all capitals.
+    static const int CONSTANTS_NAMES
+//----------------------------
+from:
+    // Problem: Hard to comment sections of files without using an IDE to do it for you
+    /* */
+    /**
+     * 
+     */
+    //
+    code();    // too many spaces after code
+to:
+    // Able to easily comment entire section during testing using /* */
+    //
+    // 
+    //
+    //
+    code(); // one space is more than enough to be clear
+//----------------------------
+from: 
+    // todo this
+    // fix this
+to:
+    // Adopted from Java which is pretty universal
+    // TODO:
+    // FIXME:
+//----------------------------
+from:
+    class A {
+        public:
+            int publicA;
+        private:
+            int privateA;
+    };
+to:
+    // Only 1 space needed for 'private' & 'public' so don't have to indent twice for members
+    class A {
+     public:
+        int publicA;
+     private:
+        int privateA;
+    };
+//----------------------------
+from:
+    fileName.h
+    fileName.cpp
+    fileNameTest.cpp
+to:
+    // No capital alphabets, and add '_' for test
+    filename.h
+    filename.cpp
+    filename_test.cpp
 //----------------------------
