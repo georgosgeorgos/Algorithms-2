00b384cafa05249815726e5e978cea69102b48fa
scheeloong <scheeloong@gmail.com>
2016-07-29 16:41:55 -0400

    Added product manager

---
+++ InterviewPractice/BrainTeaser/productmanager.cpp
@@ -0,0 +1,25 @@
+User Privacy
+    Options to opt out of data collection. 
+    The right to be forgotten. 
+    Life is on the line, location of user etc. 
+    Data can still be targeted: 
+        Postal code, male, age etc. filter until one individual. 
+        Privacy is important especially for celebrities. 
+        Use user data for user benefit
+        Strong privacy practices ( use https instead of http)
+        Make user data collection transparent
+        Give user meaningful choices to protect privacy 
+        Be responsible for information we hold. 
+        Street view had pictures of users and license plates (blurred) 
+UX
+    User Experience:
+        Ads 
+        ENG, PM, UX
+        You’re not the user. 
+        Size of keyboard (change size reduce typing error rate) 
+        Prototype & Iterate
+        Details Matter (e.g. Keyboard size) 
+        User’s don’t know what they want, just asking doesn’t work. 
+        User wants to get from point A to B, 
+        BUT user will just say faster horse, but you come up with car. 
+        Get root to problem by asking recursive “Why’s”
