0b2afa415444fc7b5e7c9f54ffc05aa84603e465
scheeloong <scheeloong@gmail.com>
2016-07-31 16:32:44 -0400

    Added notes

--- InterviewPractice/BrainTeaser/productmanager.cpp
+++ InterviewPractice/BrainTeaser/productmanager.cpp
@@ -1,3 +1,28 @@
+Product Manager:
+    Data Analysis
+    Lots of meetings with people
+    Product Requirement Documents
+    Emails
+    File bugs
+    Good PM:
+        Leadership: Influence without authority
+            Focus on user => Represent users in meetings
+            Trends => Where industry is going
+            Thinking outside the box
+            Random product assignment
+            Question types:
+                Estimation/Analytic Question
+                    E.g. estimate yellow taxis in Manhattan
+                Product Design Question
+                Design
+                User
+                Strategy Question
+                Measure product 
+                    E.g. what google should focus on for growth
+                    E.g. why would Facebook acquire Instagram
+                Book: Work Rules by Laszlo Bock, Cracking PM Interview
+                Use different products and compare strength vs weakness
+                    Gizmodo, product hunt
 User Privacy
     Options to opt out of data collection. 
     The right to be forgotten. 
@@ -23,3 +48,63 @@ UX
         User wants to get from point A to B, 
         BUT user will just say faster horse, but you come up with car. 
         Get root to problem by asking recursive “Why’s”
+        Understanding experience of users. 
+        What resources are available to you. 
+    Accessibility: 
+        Every single device
+        Every part of the world & language & culture
+        Every internet speed. 
+        Every use case
+        Every privacy 
+        Competition is only 1 click away. 
+        Company has a terrible wifi connection on purpose (drops 15 packets on purpose) 
+        Takes 30 seconds to load youtube
+        Internationalization
+        Machine Intelligence 
+        Next Billion Users
+        Find use case
+        Open Source and IP
+        Privacy & Security
+        User Experience
+        Figure out what user wants. 
+        A/B Experiment
+        Data-driven 
+        Getting details and pictures for ux is similar to coding for engineers. 
+        If do not measure before, there is no after. 
+        Record benchmarking so that you can show results after. 
+        Given users with disability alternate ways to access products. 
+        Users with 3rd party assistive technologies that aren’t supported. 
+        Assistive Technologies:
+            Sip & Puff
+            Captioning 
+            Braille Display 
+            Screen reader
+        Button shouldn’t be an image, but should be a button (using html’s button tag) so that accessibility can use it. 
+    Experience: 
+        Thought it wasn’t working at first. 
+        Try using the accessibility assuming you have bad accessibility problems. 
+        There’s no replacement to staring at the user in person.
+        Design the IDEAL Wallet
+        Design the IDEAL Wallet for blind people in Asian contries. 
+    User: 
+        Poor
+        Uses cash instead of cards 
+        Can’t idenfity how much money they are holding
+        Notice how the goal is still the same:
+        Store money
+        But the design of product changes completely
+        Work with all the teams. 
+        Top 10 apps at every country is different. 
+    Internationalization:
+        Name + “ added “ + number + “ person to his circles. “
+        Problems:
+            What if number > 1 ? => “persons”
+            What if gender(name)==girl? => “her”
+            UTF-8 and not UNICODE 
+            Sorting of names is different in other languages. 
+            ICU Library 
+            Red colored money on screen in China is good, but red colored money on screen in US is bad 
+            Make sure your message is translatable and cultural acceptable. 
+            Super long words in other languages may not fit in screens. 
+            Internationalization Libraries for formatting library. 
+            Muslim don’t like default picture to be substituted to a puppy. 
