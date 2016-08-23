dfcb6487a5bf7053e79c5c432f35b31e9d2c92a3
Soon Chee Loong <scheeloong@gmail.com>
2016-08-23 09:09:54 -0400

    Reformatted

--- InterviewPractice/MachineLearning/machinelearning.cpp
+++ InterviewPractice/MachineLearning/machinelearning.cpp
@@ -1,6 +1,7 @@
-/*
-
-Algorithms:
+//----------------------------------------------------------------------------------------------------------------------------------
+// Algorithms:
+//---------------------------------
+/* //
     Regression = measure of error in prediction made by model
         Ordinary Least Square Regression (OLSR)
         Linear Regression
@@ -8,16 +9,19 @@ Algorithms:
         Stepwise Regression
         Multivariate Adaptive Regression Splines (MARS) 
         Locally Estimated Scatterplot Smoothing
+//---------------------------------
     Instance-based = Similarities between new data and model database
         k-Nearest Neighbour (kNN)
         Learning Vector Quantization
         Self-Organizing Map
         Locally Weighted Learning
+//---------------------------------
     Regularization 
         Ridge Regression
         Least Absolute Shrinkage and Selection Operator (LASSO)
         Elastic Net
         Least-Angle Regression
+//---------------------------------
     Decision Tree
         Classification and Regression Tree (CART) 
         Iterative Dichotomiser 3 (ID3)
@@ -25,6 +29,7 @@ Algorithms:
         Decision Stump
         M5
         Conditional Decision Trees
+//---------------------------------
     Bayesian 
         Naive Bayes
         Gaussian Naive Bayes
@@ -32,24 +37,29 @@ Algorithms:
         Averaged One-Dependence Estimators (AODE)
         Bayesian Belief Network (BBN)
         Bayesian Network (BN)
+//---------------------------------
     Clustering
         k-Means
         k-Medians
         Expectation Maximisation (EM) 
         Hierarchical Clustering
+//---------------------------------
     Association Rule Learning
         Apriori Algorithm
         Eclat Algorithm
+//---------------------------------
     Artificial Neural Network
         Perceptron
         Back-Propagation
         Hopfield Network
         Radial Basis Function Network
+//---------------------------------
     Deep Learning 
         Deep Boltzmann Machine (DBM)
         Deep Belief Networks (DBN)
         Convolutional Neural Network (CNN)
         Stacked Auto-Encoders
+//---------------------------------
     Dimensionality Reduction
         Principal Component Analysis
         Principal Component Regression
@@ -61,6 +71,7 @@ Algorithms:
         Mixture Discriminant Analysis (MDA) 
         Quadratic Discriminant Analysis (QDA)
         Flexible Discriminant Analysis (FDA)
+//---------------------------------
     Ensemble
         Boosting
         Bootstrapped Aggregation (Bagging)
@@ -69,14 +80,21 @@ Algorithms:
         Gradient Boosting Machines (GBM)
         Gradient Boosted Regression Trees (GBRT)
         Random Forest
+//---------------------------------
     Support Vector Machines
+//---------------------------------
     Probabilistic Graphical Models
+//---------------------------------
 TODO:
-Support Vector Machines
-Convolutional Neural Networks 
-Recurrent Neural Networks 
-Decision Trees
-Random Forest
-Principal Component Analysis 
+    Support Vector Machines
+    Convolutional Neural Networks 
+    Recurrent Neural Networks 
+    Decision Trees
+    Random Forest
+    Principal Component Analysis 
 // */
+//----------------------------------------------------------------------------------------------------------------------------------
+// 1 Naive Bayes Classifier
+//---------------------------------
 
+//----------------------------------------------------------------------------------------------------------------------------------
