a8b1347d2b27bc6f1fe55743a71ce0c87b2a4048
scheeloong <scheeloong@gmail.com>
2016-09-07 20:35:37 -0400

    Init ML.py

---
+++ InterviewPractice/MachineLearning/machinelearning.py
@@ -0,0 +1,124 @@
+#----------------------------------------------------------------------------------------------------------------------------------
+# Algorithms:
+#---------------------------------
+# TODO: Use numpy and tensorflow. Use as much matrices notation instead of for loop as possible.
+# TODO: BELOW ARE ALL THAT YOU LEARNT A HUGE AMOUNT AND SHOULD BE DONE IN ORDER
+"""
+Algorithms
+1. Linear Regression
+2. Logistic Regression
+3. k-Nearest Neighbour
+4. Naive Bayes
+5. Backpropagation using Sigmoid + Cross Entropy
+6. Backpropagation using Softmax + Log Likelihood
+7. Backpropagation + Dropout
+8. Convolutional Neural Network
+9. Residuel Network (ResNet)
+10. Recurrent Neural Network
+11. Recurrent Neural Network + Long Short Term Memory
+12. Deep Belief Networks
+
+Data Augmentation => To increase number of data
+Transfer Learning => To solve a new problem for a dataset that's already trained on existing algo.
+
+"""
+
+# TODO: NOTE: BELOW ARE ALL THAT YOU HEARD OF AND SHOULD BE DONE THROUGHOUT YOUR CAREER
+"""
+    Regression = measure of error in prediction made by model
+        Ordinary Least Square Regression (OLSR)
+        Linear Regression
+        Logistic Regression
+        Stepwise Regression
+        Multivariate Adaptive Regression Splines (MARS) 
+        Locally Estimated Scatterplot Smoothing
+#---------------------------------
+    Instance-based = Similarities between new data and model database
+        k-Nearest Neighbour (kNN)
+        Learning Vector Quantization
+        Self-Organizing Map
+        Locally Weighted Learning
+#---------------------------------
+    Regularization 
+        Ridge Regression
+        Least Absolute Shrinkage and Selection Operator (LASSO)
+        Elastic Net
+        Least-Angle Regression
+#---------------------------------
+    Decision Tree
+        Classification and Regression Tree (CART) 
+        Iterative Dichotomiser 3 (ID3)
+        Chi-squared Automatic Interaction Detection (CHAID)
+        Decision Stump
+        M5
+        Conditional Decision Trees
+#---------------------------------
+    Bayesian 
+        Naive Bayes
+        Gaussian Naive Bayes
+        Multinomial Naive Bayes
+        Averaged One-Dependence Estimators (AODE)
+        Bayesian Belief Network (BBN)
+        Bayesian Network (BN)
+#---------------------------------
+    Clustering
+        k-Means
+        k-Medians
+        Expectation Maximisation (EM) 
+        Hierarchical Clustering
+#---------------------------------
+    Association Rule Learning
+        Apriori Algorithm
+        Eclat Algorithm
+#---------------------------------
+    Artificial Neural Network
+        Perceptron
+        back-propagation (both sigmoid + cross entropy, softmax + loglikelihood) 
+        hopfield network
+        Radial Basis Function Network
+#---------------------------------
+    Deep Learning 
+        Fully Connected Neural Network
+        Convolutional Neural Network (CNN)
+        Recurrent Neural Network (RNN)
+        Long Short Term Memory (LSTM)
+        Deep Boltzmann Machine (DBM)
+        Deep Belief Networks (DBN)
+        Stacked Auto-Encoders
+#---------------------------------
+    Dimensionality Reduction
+        Principal Component Analysis
+        Principal Component Regression
+        Partial Least Square Regression (PLSR)
+        Sammon Mapping
+        Multidimensional Scaling (MDS) 
+        Projection Pursuit
+        Linear Discriminant Analysis (LDA)
+        Mixture Discriminant Analysis (MDA) 
+        Quadratic Discriminant Analysis (QDA)
+        Flexible Discriminant Analysis (FDA)
+#---------------------------------
+    Ensemble
+        Boosting
+        Bootstrapped Aggregation (Bagging)
+        AdaBoost
+        Stacked Generalization (blending)
+        Gradient Boosting Machines (GBM)
+        Gradient Boosted Regression Trees (GBRT)
+        Random Forest
+#---------------------------------
+    Support Vector Machines
+#---------------------------------
+    Probabilistic Graphical Models
+#---------------------------------
+2-class Classification (Email spam or not spam)
+    Solved using:
+        Logistic Regression (if data is poisson)
+        Gaussian Discriminant Analysis (if data is in fact gaussian)
+#---------------------------------
+"""
+#----------------------------------------------------------------------------------------------------------------------------------
+# 1 Naive Bayes Classifier
+#---------------------------------
+
+#----------------------------------------------------------------------------------------------------------------------------------
