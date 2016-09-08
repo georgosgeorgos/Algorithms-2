67b98f8979b7c5159feb959cacdc7d24721e0e1c
scheeloong <scheeloong@gmail.com>
2016-09-07 20:35:05 -0400

    Added notes

--- InterviewPractice/LanguageSpecific/languageSpecific.py
+++ InterviewPractice/LanguageSpecific/languageSpecific.py
@@ -1,13 +1,23 @@
 #----------------------------------------------------------------------------------------------------------------------------------
 # Headers and Libraries
 #-------------------------
+# Built-in
+import random
+# Testing
+import unittest
+import mock
+# Others
 import numpy as np # Working with matrices, numerical python
 import tensorflow as tf # Working with Machine Learning
 import pickle # Serialization and De-serialization (python's version of json)
+import cPickle # Reading files
+import gzip # Opening .gz files
 # Databases
 import sqlalchemy # For working with sql
 # Compilation to other languages
 import Cython # Convert python to C++ 
+import Jython # Convert python to Java
+import IronPython # Convert python to C#
 # Web Framework
 import django # Web Framework
 import scrapy # Web scraper
@@ -49,6 +59,10 @@ class NameOfClass(object):
   # Similar to toString() in Java
   def __str__(self):
     return "%s , %s" % (self.firstMemberVariable, self.secondMemberVariable)
+
+  # Returns next iterator
+  def __next__(self):
+      raise NotImplementedError
 
 # Defining a child class
 class ChildClass(NameOfClass):
