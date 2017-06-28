9c43ef8ca5c096e12cef082a468b86c7dc797711
scheeloong <scheeloong@gmail.com>
2017-06-28 15:16:54 -0400

    Addes notes on jupyter

--- InterviewPractice/LanguageSpecific/languageSpecific.py
+++ InterviewPractice/LanguageSpecific/languageSpecific.py
@@ -6,9 +6,7 @@ sudo apt-get update
 sudo apt-get install pip # Installing any library
 sudo pip install tensorflow
 sudo pip install anyPythonLibraryName
-# Installing from a requirements.txt
-sudo pip install -r requirements.txt
-# Installing from setup.py
+# Installing from a requirements.txt sudo pip install -r requirements.txt # Installing from setup.py
 sudo pip install .
 
 #----------------------------------------------------------------------------------------------------------------------------------
@@ -149,5 +147,10 @@ class ChildClass(NameOfClass):
 #---------------------------------------------------------------------------------------------------
 # Concurrency
 #-------------------------
-
+#---------------------------------------------------------------------------------------------------
+# Jupyter Notebook
+#-------------------------
+An advantage of jupyter notebook, which behaves like MatLab. 
+So lets just say it’s training while reading variables batchSize, learningRate, etc. 
+You can run the model.train code in a different terminal window in jupyter notebook. Then, pause the execution of the model.train code, then re-run the terminal that defines the values of variables batchSize, learningRate, etc, which is in a different terminal window than model.train, then resume model.train code, it will now read the latest variable values that were defined. 
 #---------------------------------------------------------------------------------------------------
