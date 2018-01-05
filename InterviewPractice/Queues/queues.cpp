c71532eaf94a5a46059869e65be23e2b9ac684a4
scheeloong <scheeloong@gmail.com>
2018-01-04 20:26:26 -0500

    Fixed code that was wrong with maxContiguousKSubArrayElements with queues, you are actually getting better at testing code :D

--- InterviewPractice/Queues/queues.cpp
+++ InterviewPractice/Queues/queues.cpp
@@ -2,12 +2,13 @@
 //----------------------------------------------------------------------------------------
 /* //
 Table of Contents
-1. Find Maximum of Every Contiguous k Subarray Elements in An Array, T(n,k) = O(n), S(n,k) = O(k)
+1. Find Maximum of Every Contiguous k Subarray Elements in An Array, T(n,k) = O(2n), S(n,k) = O(k)
 
 TODO: Implement LRU Cache
 // */
 //----------------------------------------------------------------------------------------
 /* //
+note: QUEUE PUSHES TO BACK AND POPS FROM FRONT!
 #include <queue>
     // q.empty()
     // q.size()
@@ -52,6 +53,7 @@ Deque = Double Ended Queue => Queue + Stack
 // Common Hints
 //-------------------------------------------
     Instead of storing the values, try storing the indices. 
+    Instead of strong everything, only store the values you need.
 //-------------------------------------------
 // Common Questions
 //-------------------------------------------
@@ -59,7 +61,7 @@ Deque = Double Ended Queue => Queue + Stack
 // */
 //----------------------------------------------------------------------------------------
 // 1 Find Maximum of Every Contiguous k Subarray Elements in An Array
-// Time Complexity, T(n,k) = O(n)
+// Time Complexity, T(n,k) = O(2n)
 // Space Complexity, S(n,k) = O(k)
 //-------------------------------------------
 /*
@@ -77,6 +79,7 @@ TestCases:
     [1, 3, 2, 4], k = 2 => [3, 3, 4]
                   k = 3 => [3, 4]
                   k = 10 => [4]
+    [8, 2, 5, 4, 3], k = 4 => [8, 5] // next largest is in middle, which is 5
 Algorithm:
     Solution 1: Push and pop from queue, checking queue each iteration, T(n,k) = O(nk), S(n,k) = O(k)
         (n iteration), each iteration need O(k) to check max)
@@ -98,16 +101,63 @@ Algorithm:
         Can always pop whatever that is smaller than current window
         But must always keep track whatever smaller than later window
         Get rid by popping the larger elements that have lower indices than (current iteration - k)
+        Note: The time complexity is at worst 2n since for each element 
+        you will at most add them once and pop them once, so 2n is worst case, T(2n) = T(n) 
 Implement!
 Test!
 */
 //-------------------------------------------
 /* //
 #include <deque>
+#include <queue>
 #include <algorithm> // std::min()
 #include <vector>
 #include <iostream>
 using namespace std;
+
+vector<int> MaxContiguousSubarrayElements(const vector<int>& arr, int numContiguousElement)
+{
+    vector<int> solution; 
+    if (arr.empty() || numContiguousElement <= 0) return solution;
+    // Ensure we do not loop pass the array's size
+    numContiguousElement = min(numContiguousElement, (int) arr.size());
+    // A queue that stores the current maximum at the end of the queue 
+    // end is the front of the queue in C++ STL's #include <queue>
+    queue<int> currMaxSoFarAtEnd; 
+    // Initialize the queue for the first interval
+    for (int i = 0; i < numContiguousElement; i++)
+    {
+        currMaxSoFarAtEnd.push(arr[i]); // pushes to back of queue
+        // note: Add duplicates if they exist, use '<' instead of '<='
+        while(currMaxSoFarAtEnd.front() < arr[i])
+        {
+            currMaxSoFarAtEnd.pop(); // pops from front of queue
+        }
+    }
+
+    // Append the current maximum of the first interval
+    solution.push_back(currMaxSoFarAtEnd.front());
+
+    // For every remaining intervals
+    for (int currStartIndex = 1; currStartIndex + numContiguousElement - 1 < arr.size(); currStartIndex++) 
+    {
+        int deletedIndex = currStartIndex - 1;
+        // Get rid of outdated maximum
+        if (currMaxSoFarAtEnd.front() == arr[deletedIndex]) currMaxSoFarAtEnd.pop();
+        // Add the new item
+        int currLatestItemIndex = currStartIndex +  numContiguousElement - 1;
+        currMaxSoFarAtEnd.push(arr[currLatestItemIndex]);
+        // Get rid of non-maximum 
+        // note: Add duplicates if they exist, so use '<' instead of '<='
+        while(currMaxSoFarAtEnd.front() < arr[currLatestItemIndex])
+        {
+            currMaxSoFarAtEnd.pop();
+        }
+        // Append the current maximum of the interval
+        solution.push_back(currMaxSoFarAtEnd.front());
+    }
+    return solution;
+}
 
 // Gets rid of the smaller values from older indexes from back
 void getRidOfOlderSmallerFromBack(const vector<int>& arr, const int& currentVal, deque<int>& maxOfInterval)
@@ -122,10 +172,14 @@ void getRidOfOutdatedFromFront(const int& oldestIndex, deque<int>& maxOfInterval
         maxOfInterval.pop_front();
 } 
 
-vector<int> MaxContiguousSubarrayElements(const vector<int>& arr, int numContiguousElement)
+vector<int> MaxContiguousSubarrayElementsWrong(const vector<int>& arr, int numContiguousElement)
 {
+    // LOL, this is WRONG! It fails the test case below
+    // vector<int> nextLargestInMiddle = {8, 2, 5, 4, 3} with numContiguousElement = 4
+    // should return 8, 5. 
+    // You fixed it with the latest implementation that is much better than this.
     vector<int> solution;
-    if(arr.empty() || numContiguousElement <= 0) return solution;
+    if (arr.empty() || numContiguousElement <= 0) return solution;
     // Front stores the largest element in current interval, back stores the smallest element in current interval
     // It doesn't store the actual value, but the indices of the values from the arr
     // Thus, to get the current maximum, do arr[maxOfInterval.front()];
@@ -156,8 +210,11 @@ vector<int> MaxContiguousSubarrayElements(const vector<int>& arr, int numContigu
 
 void printSolution(vector<int>& arr, int numContiguousElements) 
 {
+    cout << "Input: [ ";
+    for (auto val : arr) cout << val << " ";
+    cout << "], numContigous = " << numContiguousElements << " => ";
     vector<int> solution = MaxContiguousSubarrayElements(arr, numContiguousElements);
-    for(int val : solution) cout << val << " ";
+    for(auto val : solution) cout << val << " ";
     cout << endl;
 }
 
@@ -166,11 +223,13 @@ int main(void)
     vector<int> empty;
     vector<int> single = {1};
     vector<int> normal = {1, 3, 2, 4};
+    vector<int> nextLargestInMiddle = {8, 2, 5, 4, 3};
     printSolution(empty, 100);
     printSolution(single, 100); // [1]
     printSolution(normal, 2); // [3, 3, 4]
     printSolution(normal, 3); // [3, 4]
     printSolution(normal, 10); // [4]
+    printSolution(nextLargestInMiddle, 4); // [8, 5]
     return 0;
 }
 // */
