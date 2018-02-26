e8ca0d8b6a61e908270f0f6a2378368f739da396
scheeloong <scheeloong@gmail.com>
2018-02-25 22:41:28 -0500

    Implemented Coordinate Sparse Matrix

--- InterviewPractice/SparseMatrix/sparsematrix.cpp
+++ InterviewPractice/SparseMatrix/sparsematrix.cpp
@@ -1,7 +1,7 @@
 //----------------------------------------------------------------------------------------------------------------------------------
 /* //
 Table of Contents
-1.TODO Algorithm, T(n) = O(n), S(n) = O(n)
+1. Coordinate Format Sparse Matrix
 //-------------------------
 TODO:
 Compressed Sparse Row
@@ -13,9 +13,9 @@ Dictionary of Keys
 Row-based Linked List
 //----------------------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------------------
-// 1 
-// Time Complexity, T(n) = O(n)
-// Space Complexity, S(n) = O(n)
+// 1 Coordinate Format Sparse Matrix
+// Time Complexity, T(n) = TODO
+// Space Complexity, S(n) = TODO
 //---------------------------------
 /*
 Questions:
@@ -23,14 +23,52 @@ TestCase:
 Algorithm:
 */
 //---------------------------------
-/* //
-#include <limits> // numeric_limits
+//
 #include <vector>
 #include <iostream>
 using namespace std;
+
+class SparseMatrixCoordinate {
+public:
+    SparseMatrixCoordinate(vector<vector<int>>& matrix);
+    ~SparseMatrixCoordinate();
+private:
+    vector<int> row;
+    vector<int> col;
+    vector<int> val;
+};
+
+SparseMatrixCoordinate::SparseMatrixCoordinate(vector<vector<int>>& matrix) 
+{
+    for (int rowIndex = 0; rowIndex < matrix.size(); rowIndex++)
+    {
+        for (int colIndex = 0; colIndex < matrix[rowIndex].size(); colIndex++)
+        {
+            int currVal = matrix[rowIndex][colIndex];
+            if (currVal != 0)
+            {
+                row.push_back(rowIndex);
+                col.push_back(colIndex);
+                val.push_back(currVal);
+            }
+        }
+    }
+}
+
+SparseMatrixCoordinate::~SparseMatrixCoordinate() {}
+
 int main(void)
 {
-    vector<int> empty; // empty: 0
+    int N = 5;
+    vector<vector<int>> sparseMatrix(N, vector<int>(N, 0));
+    sparseMatrix[0][1] = 2;
+    sparseMatrix[0][4] = 3;
+    sparseMatrix[1][3] = 7;
+    sparseMatrix[2][1] = 8;
+    sparseMatrix[3][3] = 9;
+    sparseMatrix[4][3] = 1;
+    SparseMatrixCoordinate smc(sparseMatrix);
+    cout << "done" << endl;
 }
 // */
 //----------------------------------------------------------------------------------------------------------------------------------
